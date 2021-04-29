95e30174d1d6d62cd016785aa857843d6024dc16
tmadar <travismadar@gmail.com>
2021-04-29 08:10:07 -0700

    fix limit options

--- test/pagination_param_validator_test.exs
+++ test/pagination_param_validator_test.exs
@@ -8,77 +8,77 @@ defmodule Solicit.Plugs.Validation.PaginationParamTest do
     test "offset and page provided, expect error" do
       build_conn(:get, "?offset=1&page=1")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "offset provided not a number, expect error" do
       build_conn(:get, "?offset=abc123")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "page provided not a number, expect error" do
       build_conn(:get, "?page=abc123")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "limit provided not a number, expect error" do
       build_conn(:get, "?limit=abc123")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "offset provided but empty, expect error" do
       build_conn(:get, "?offset=")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "page provided but empty, expect error" do
       build_conn(:get, "?page=")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "limit provided but empty, expect error" do
       build_conn(:get, "?limit=")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "offset provided but negative number, expect error" do
       build_conn(:get, "?offset=-1")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "page provided but negative number, expect error" do
       build_conn(:get, "?page=-1")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "limit provided but negative number, expect error" do
       build_conn(:get, "?limit=-1")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
     test "limit exceeds default limit - (empty struct), expect error" do
       build_conn(:get, "?limit=999999")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{})
+      |> PaginationParam.call([])
       |> json_response(:unprocessable_entity)
     end
 
@@ -92,28 +92,28 @@ defmodule Solicit.Plugs.Validation.PaginationParamTest do
     test "limit exceeds custom limit, expect error" do
       build_conn(:get, "?limit=11")
       |> Plug.Conn.fetch_query_params()
-      |> PaginationParam.call(%{limit: 10})
+      |> PaginationParam.call([limit: 10])
       |> json_response(:unprocessable_entity)
     end
 
     test "offset provided" do
       conn = Plug.Conn.fetch_query_params(build_conn(:get, "?offset=1"))
-      assert conn == PaginationParam.call(conn, %{})
+      assert conn == PaginationParam.call(conn, [])
     end
 
     test "page provided" do
       conn = Plug.Conn.fetch_query_params(build_conn(:get, "?page=1"))
-      assert conn == PaginationParam.call(conn, %{})
+      assert conn == PaginationParam.call(conn, [])
     end
 
     test "offset and limit provided" do
       conn = Plug.Conn.fetch_query_params(build_conn(:get, "?offset=1&limit=1"))
-      assert conn == PaginationParam.call(conn, %{})
+      assert conn == PaginationParam.call(conn, [])
     end
 
     test "page and limit provided" do
       conn = Plug.Conn.fetch_query_params(build_conn(:get, "?page=1&limit=1"))
-      assert conn == PaginationParam.call(conn, %{})
+      assert conn == PaginationParam.call(conn, [])
     end
   end
 end
