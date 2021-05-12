fef9b0288e50b03d85445687e2cc60257e906c3a
tmadar <travismadar@gmail.com>
2021-05-12 09:06:25 -0700

    fix type in spec

--- lib/plugs/query_params_validator.ex
+++ lib/plugs/query_params_validator.ex
@@ -9,7 +9,7 @@ defmodule Solicit.Plugs.Validation.QueryParams do
   @spec init(keyword()) :: keyword()
   def init(opts \\ []), do: opts
 
-  @spec call(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  @spec call(Plug.Conn.t(), keyword()) :: Plug.Conn.t()
   def call(%Plug.Conn{query_params: query_params} = conn, _options) when query_params != %{} do
     filtered_params =
       Enum.reduce(query_params, %{}, fn {key, value}, acc ->
