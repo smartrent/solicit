9d608f2333123e46a1d9ae7579a95699f32a8c95
tmadar <travismadar@gmail.com>
2021-05-31 08:59:43 -0700

    wip

--- test/pagination_params_validator_test.exs
+++ test/pagination_params_validator_test.exs
@@ -5,108 +5,135 @@ defmodule Solicit.Plugs.Validation.PaginationParamssTest do
   alias Solicit.Plugs.Validation.PaginationParams
 
   describe "call" do
-    test "offset and page provided, expect error" do
-      build_conn(:get, "?offset=1&page=1")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
+    dataprovider = [
+      %{
+        test_scenario: "offset and page provided, expect error",
+        value: "?offset=1&page=1",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "offset provided not a number, expect error",
+        value: "?offset=abc123",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "page provided not a number, expect error",
+        value: "?page=abc123",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "offset provided but empty, expect error",
+        value: "?offset=",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "page provided but empty, expect error",
+        value: "?page=",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "offset provided but negative number, expect error",
+        value: "?offset=-1",
+        expectation: :unprocessable_entity
+      },
+      %{
+        test_scenario: "page provided but negative number, expect error",
+        value: "?page=-1",
+        expectation: :unprocessable_entity
+      }
+    ]
 
-    test "offset provided not a number, expect error" do
-      build_conn(:get, "?offset=abc123")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
-
-    test "page provided not a number, expect error" do
-      build_conn(:get, "?page=abc123")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
-
-    test "limit provided not a number, expect error" do
-      %{params: %{"limit" => 1000}} =
-        build_conn(:get, "?limit=abc123")
+    Enum.each(dataprovider, fn %{
+                                 test_scenario: test_scenario,
+                                 value: value,
+                                 expectation: expectation
+                               } ->
+      @test_scenario test_scenario
+      @value value
+      @expectation expectation
+      test "#{@test_scenario}" do
+        build_conn(:get, @value)
         |> Plug.Conn.fetch_query_params()
         |> PaginationParams.call([])
-    end
+        |> json_response(@expectation)
+      end
+    end)
 
-    test "offset provided but empty, expect error" do
-      build_conn(:get, "?offset=")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
+    dataprovider = [
+      %{
+        test_scenario: "limit provided not a number returns default limit",
+        value: "?limit=abc123",
+        expectation: %{params: %{"limit" => 1000}}
+      },
+      %{
+        test_scenario: "limit provided but empty returns default limit",
+        value: "?limit=",
+        expectation: %{params: %{"limit" => 1000}}
+      },
+      %{
+        test_scenario: "limit provided but negative number returns default limit",
+        value: "?limit=-1",
+        expectation: %{params: %{"limit" => 1000}}
+      },
+      %{
+        test_scenario: "limit exceeds default limit ceiling returns default limit",
+        value: "?limit=999999",
+        expectation: %{params: %{"limit" => 1000}}
+      }
+    ]
 
-    test "page provided but empty, expect error" do
-      build_conn(:get, "?page=")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
+    Enum.each(dataprovider, fn %{
+                                 test_scenario: test_scenario,
+                                 value: value,
+                                 expectation: expectation
+                               } ->
+      @test_scenario test_scenario
+      @value value
+      @expectation expectation
+      test "#{@test_scenario}" do
+        @expectation =
+          build_conn(:get, @value)
+          |> Plug.Conn.fetch_query_params()
+          |> PaginationParams.call([])
+      end
+    end)
 
-    test "limit provided but empty, expect error" do
-      %{params: %{"limit" => 1000}} =
-        build_conn(:get, "?limit=")
-        |> Plug.Conn.fetch_query_params()
-        |> PaginationParams.call([])
-    end
-
-    test "offset provided but negative number, expect error" do
-      build_conn(:get, "?offset=-1")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
-
-    test "page provided but negative number, expect error" do
-      build_conn(:get, "?page=-1")
-      |> Plug.Conn.fetch_query_params()
-      |> PaginationParams.call([])
-      |> json_response(:unprocessable_entity)
-    end
-
-    test "limit provided but negative number, expect error" do
-      %{params: %{"limit" => 1000}} =
-        build_conn(:get, "?limit=-1")
-        |> Plug.Conn.fetch_query_params()
-        |> PaginationParams.call([])
-    end
-
-    test "limit exceeds default limit - (empty struct), expect error" do
-      %{params: %{"limit" => 1000}} =
-        build_conn(:get, "?limit=999999")
-        |> Plug.Conn.fetch_query_params()
-        |> PaginationParams.call([])
-    end
-
-    test "limit exceeds custom limit, expect error" do
+    test "limit exceeds custom limit returns custom limit" do
       %{params: %{"limit" => 10}} =
         build_conn(:get, "?limit=11")
         |> Plug.Conn.fetch_query_params()
         |> PaginationParams.call(max_limit: 10)
     end
 
-    test "offset provided" do
-      conn = Plug.Conn.fetch_query_params(build_conn(:get, "?offset=1"))
-      assert conn == PaginationParams.call(conn, [])
-    end
+    dataprovider = [
+      %{
+        test_scenario: "offset provided",
+        value: "?offset=1"
+      },
+      %{
+        test_scenario: "page provided",
+        value: "?page=1"
+      },
+      %{
+        test_scenario: "offset and limit provided",
+        value: "?offset=1&limit=1"
+      },
+      %{
+        test_scenario: "page and limit provided",
+        value: "?page=1&limit=1"
+      },
+    ]
 
-    test "page provided" do
-      conn = Plug.Conn.fetch_query_params(build_conn(:get, "?page=1"))
-      assert conn == PaginationParams.call(conn, [])
-    end
-
-    test "offset and limit provided" do
-      conn = Plug.Conn.fetch_query_params(build_conn(:get, "?offset=1&limit=1"))
-      assert conn == PaginationParams.call(conn, [])
-    end
-
-    test "page and limit provided" do
-      conn = Plug.Conn.fetch_query_params(build_conn(:get, "?page=1&limit=1"))
-      assert conn == PaginationParams.call(conn, [])
-    end
+    Enum.each(dataprovider, fn %{
+                                 test_scenario: test_scenario,
+                                 value: value
+                               } ->
+      @test_scenario test_scenario
+      @value value
+      test "#{@test_scenario}" do
+        conn = Plug.Conn.fetch_query_params(build_conn(:get, @value))
+        assert conn == PaginationParams.call(conn, [])
+      end
+    end)
   end
 end
